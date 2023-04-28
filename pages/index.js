55a602c05461d9a905794c8e7bf623b316192311
Weston Nelson <dogooddowell@protonmail.com>
2023-04-27 22:17:35 -0500

    Update index.js
    Updates
    
    Signed-off-by: Weston Nelson <dogooddowell@protonmail.com>

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
         <p className={styles.description}>
           Get started by staking your $NFTE tokens.
-          <code className={styles.code}>pages/_app.js</code>, then watch your $ARB rewards grow!
+          <code className={styles.code}> and then, </code> watch your $ARB rewards grow!
         </p>
 
         <div className={styles.connect}>
@@ -21,27 +21,26 @@ export default function Home() {
         </div>
 
         <div className={styles.grid}>
-          <a href="https://arbiscan.io/" className={styles.card}>
+          <a href="https://arbiscan.io/address/0xaa9d9a8f810463f24825c05f3075c1fc6a430430#internaltx/" className={styles.card}>
             <h2>Arbiscan &rarr;</h2>
             <p>
-              Arbitrum block explorer
+              View the staking contract on the Arbitrum block explorer
             </p>
           </a>
 
-          <a href="https://docs.nftearth.exchange" className={styles.card}>
-            <h2>Docs &rarr;</h2>
-            <p>
-              NFTEarth docs
-            </p>
-          </a>
-
-          <a
+         <a
             href="https://nftearth.exchange"
             className={styles.card}
           >
             <h2>Marketplace &rarr;</h2>
             <p>
               Trade NFTs on L2
+            </p>
+
+          <a href="https://docs.nftearth.exchange" className={styles.card}>
+            <h2>Docs &rarr;</h2>
+            <p>
+              NFTEarth docs
             </p>
           </a>
         </div>
