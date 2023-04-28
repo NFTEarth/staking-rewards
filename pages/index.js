e03c444c22feca9fb9627ff6837085a5f16df5cc
Weston Nelson <dogooddowell@protonmail.com>
2023-04-27 22:19:50 -0500

    Update index.js
    Update typos
    
    Signed-off-by: Weston Nelson <dogooddowell@protonmail.com>

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,11 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to NFTE token staking rewards!
+          Welcome to NFTE token staking rewards
         </h1>
 
         <p className={styles.description}>
-          Get started by staking your $NFTE tokens.
-          <code className={styles.code}> and then, </code> watch your $ARB rewards grow!
+          Get started by staking your $NFTE tokens, and watch your $ARB rewards grow
         </p>
 
         <div className={styles.connect}>
@@ -21,10 +20,10 @@ export default function Home() {
         </div>
 
         <div className={styles.grid}>
-          <a href="https://arbiscan.io/address/0xaa9d9a8f810463f24825c05f3075c1fc6a430430#internaltx/" className={styles.card}>
+          <a href="https://arbiscan.io/address/0xaa9d9a8f810463f24825c05f3075c1fc6a430430" className={styles.card}>
             <h2>Arbiscan &rarr;</h2>
             <p>
-              View the staking contract on the Arbitrum block explorer
+              View the token staking contract on the Arbitrum block explorer
             </p>
           </a>
 
@@ -34,7 +33,7 @@ export default function Home() {
           >
             <h2>Marketplace &rarr;</h2>
             <p>
-              Trade NFTs on L2
+              Trade NFTs on L2 at the NFTEarth marketplace
             </p>
 
           <a href="https://docs.nftearth.exchange" className={styles.card}>
